2caab29a1f39db6d99b435e83b33bb55ee4e2f52
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-11-12 12:44:14 +0600

    updated current files

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 fruits = ["apples", "pears", "grapes"]
 
 vegetables = ["tomato", "potato", "onion"]
@@ -12,6 +13,7 @@ fruits.insert(0, "banana")
 # print(fruits)
 
 print(fruits.index("pears"))
+print("pears" in fruits)
 print(type(fruits))
 
 array = np.array([1, 2, 3, 4, 5, 6])
