34d02d7d0eaaebad4ebe64380763f4a38199549f
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-11-12 12:40:47 +0600

    Added new program

---
+++ practice.py
@@ -0,0 +1,19 @@
+import numpy as np
+fruits = ["apples", "pears", "grapes"]
+
+vegetables = ["tomato", "potato", "onion"]
+
+fruits.append("peach")
+
+# print(fruits)
+
+fruits.insert(0, "banana")
+
+# print(fruits)
+
+print(fruits.index("pears"))
+print(type(fruits))
+
+array = np.array([1, 2, 3, 4, 5, 6])
+print(array)
+print(type(array))
