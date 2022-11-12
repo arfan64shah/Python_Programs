0f7fb2d037a217f0dbeccc120c0cfddc1e86293e
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-11-13 00:43:56 +0600

    Added degrees to radians

---
+++ degree_to_radians.py
@@ -0,0 +1,9 @@
+
+import math
+degrees = int(input("Enter angle in degrees: "))
+
+def radians():
+    radians = (math.pi/180)*degrees
+    return radians
+
+print("Radians is ", radians())
--- practice.py
+++ practice.py
@@ -12,10 +12,26 @@ fruits.insert(0, "banana")
 
 # print(fruits)
 
-print(fruits.index("pears"))
-print("pears" in fruits)
-print(type(fruits))
+#print(fruits.index("pears"))
+#print("pears" in fruits)
+#print(type(fruits))
 
 array = np.array([1, 2, 3, 4, 5, 6])
-print(array)
-print(type(array))
+#print(array)
+#print(type(array))
+
+#range function 
+
+even_numbers = list(range(0, 100, 2))
+print(even_numbers)
+
+count = 0
+while(count <= 10):
+    print(count)
+    count+=1
+
+
+do {
+    print(count)
+    count+=1
+}while(count <= 10)
