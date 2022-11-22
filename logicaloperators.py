d93b62b45adea92a430056c9e05314b9425973a0
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-11-22 21:32:37 +0600

    Added program for logical operators

---
+++ logicaloperators.py
@@ -0,0 +1,16 @@
+x = 6
+
+#or operator
+if(x > 4 or x < 6):
+    print("This is True")
+else:
+    print("This is False")
+
+#and operator
+if(x > 4 and x < 6):
+    print("This is True")
+else:
+    print("This is False")
+
+#not operator
+print(not(True))
