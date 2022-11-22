1add5440dcd35a0b36b967a1f9485d0191fa4d8f
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-11-22 21:24:58 +0600

    Added new program

---
+++ assignmentoperators.py
@@ -0,0 +1,54 @@
+#lets declare a variable named as x
+
+#simple assignment
+x = 30
+
+#Add and equal operator
+x += 3
+print(x)
+
+#Subtract and equal
+x -= 3
+print(x)
+
+#multiply and equal
+x *= 3
+print(x)
+
+#divide and equal
+x /= 3
+print(x)
+
+#modulus and equal
+x %= 4
+print(x)
+
+#floor division and equal
+x //= 3
+print(x)
+
+#exponent and equal
+x **= 3
+print(x)
+
+#bitwise AND
+x &= 3
+print(x)
+
+#bitwise OR
+x |= 3
+print(x)
+
+#bitwise XOR
+x ^= 3
+print(x)
+
+#bitwise right shift
+x >>= 3
+print(x)
+
+#bitwise left shift
+x <<= 3
+print(x)
+
+
