8e45386308f7997fe7a714a48f905c307f22e65c
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-11-22 22:42:17 +0600

    Added program for bitwise operators

---
+++ bitwiseoperator.py
@@ -0,0 +1,30 @@
+x = 8
+y = 6
+
+#binary version of x and y
+print("Binary version x is ", bin(x))
+print("Binary version y is ", bin(y))
+
+#Or operator
+print("OR of x and y is ", x|y)
+print("Binary version of x|y is ", bin(x|y))
+
+#and operator
+print("AND of x and y is ", x&y)
+print("Binary version of x&y is ", bin(x&y))
+
+#XOR operator
+print("XOR of x and y is ", x^y)
+print("Binary version of x^y is ", bin(x^y))
+
+#NOT operator
+print("Reverse of x is ", ~x)
+print("Binary version of ~x is ", bin(~x))
+
+#left shift
+print("Left Shift of x by 2 is ", x << 2)
+print("Binary version of x << 2 is ", bin(x << 2))
+
+#right shift
+print("Right Shift of x by 2 is ", x >> 2)
+print("Binary version of x >> 2 is ", bin(x >> 2))
