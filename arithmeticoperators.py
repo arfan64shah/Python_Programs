9a0909d40308344bf9dbdddd3163c1e072bc01a3
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-11-22 20:07:04 +0600

    Added new program

---
+++ arithmeticoperators.py
@@ -0,0 +1,31 @@
+#declare two numbers
+num1 = 5
+num2 = 6
+
+#addition operator
+sum = num1 + num2
+print("The sum is ", sum)
+
+#subtraction operator
+sub = num1 - num2
+print("The difference is ", sub)
+
+#multiplication operator
+product = num1 * num2
+print("The product is ", product)
+
+#division operator
+division = num1 / num2
+print("The division is ", division)
+
+#modulus operator
+mod = num1 % num2
+print("Modulus of given numbers is ", mod)
+
+#exponent operator
+exp = num1 ** num2
+print("Answer after exponentiation is ", exp)
+
+#floor division
+floorDiv = num1 // num2
+print("Floor division is ", floorDiv)
--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print("Hello World!")
+a = 5
+
+a ^= 2
+print(a)
