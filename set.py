cc3fa0ee51e7abb4507da6dc4b0b628a8a5664f6
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-12-03 11:56:03 +0600

    Added conversion of list to set and vice versa

--- set.py
+++ set.py
@@ -1,3 +1,15 @@
-set = {1, 2, 3, 4, 5, 6, 1, 2}
-print(set)
-print(type(set))
+#declare a list
+set1 = {1, 2, 3, 4, 5, 6, 1, 2}
+print(set1)
+print(type(set1))
+
+#now lets declrae a list
+list1 = [1, 1,1, 2, 2, 3, 4, 5, 5, 6]
+mySet = set(list1)
+print(mySet)
+print(type(mySet))
+
+#now change the set into list
+list2 = list(mySet)
+print(list2)
+print(type(list2))
