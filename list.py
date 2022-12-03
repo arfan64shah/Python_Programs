87d4ea517f00f32951eb26d7affccb67a0f82e76
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-12-03 11:26:32 +0600

    Added new program for list

---
+++ list.py
@@ -0,0 +1,26 @@
+list = ['python', 'scipy', 2.7]
+print(list)
+
+#delete last element
+list.pop(-1)
+print(list)
+
+
+#now insert another element
+list.insert(2, 'numpy')
+print(list[0])
+
+print(list[-1])
+
+#declare a a list inside another list
+twoDimensionalList = [[1, 2], [3, 4]]
+print(twoDimensionalList[0])
+print(twoDimensionalList[1])
+
+print(twoDimensionalList[0][0])
+
+#declare a new list of strings
+namesList = ['kiran', 'adiba', 'dibar', 'wajahat', 'amin']
+namesList.sort(reverse=True)
+
+print(namesList)
