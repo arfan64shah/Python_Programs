a8b386e806ed053894020333e9a190a62cbe04f0
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-12-03 11:35:02 +0600

    Added list comprehension

--- list.py
+++ list.py
@@ -24,3 +24,12 @@ namesList = ['kiran', 'adiba', 'dibar', 'wajahat', 'amin']
 namesList.sort(reverse=True)
 
 print(namesList)
+
+#declare an list and empty list
+newList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+squares = []
+
+for i in newList:
+    squares.append(i**2)
+print(newList)
+print(squares)
