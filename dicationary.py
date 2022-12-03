78721f9f2bd9422bf6791c0d0c0c24f32b19b6fc
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-12-03 14:12:02 +0600

    Added program for dicationary

---
+++ dicationary.py
@@ -0,0 +1,7 @@
+myDict = {'name': 'Arfan Shah', 'age': '25', 'profession': 'Student', 'name': 'Arfan Shah'}
+
+print(myDict)
+print(myDict['name'])
+print(myDict['age'])
+
+print("My name is "  + myDict['name'] + " and I am " + myDict['age'] + " years old. I am a " + myDict['profession'] + " by profession.")
