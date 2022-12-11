18053fe6b7b4a38d19404b7367effc0559a89968
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-12-11 14:09:12 +0600

    Added databse for many

---
+++ manysqlite.py
@@ -0,0 +1,30 @@
+import sqlite3 as sq
+
+conn = sq.connect("manydb.db")
+cur = conn.cursor()
+
+#create a table
+
+cur.execute('''
+create table customers(
+    c_id int primary key,
+    c_fname varchar(50),
+    c_lname varchar(50),
+    c_email varchar(50)
+)
+    
+
+
+''')
+
+many_data = [
+    (1, 'arfan', 'shah', 'arfan@gmail.com'),
+    (2, 'zulfiqar', 'azam', 'zulfiqar@gmail.com'),
+    (3, 'tajwar', 'ali', 'tajwar@gmail.com'),
+    (4, 'munner', 'abbas', 'munner@gmail.com')
+]
+
+cur.executemany("insert into customers values(?, ?, ?, ?)", many_data)
+
+conn.commit()
+conn.close()
