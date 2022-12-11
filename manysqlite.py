54be52141037d6e1b06610dfe88ec6c8670fcec0
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-12-11 14:20:26 +0600

    Added execuations to database

--- manysqlite.py
+++ manysqlite.py
@@ -5,26 +5,31 @@ cur = conn.cursor()
 
 #create a table
 
-cur.execute('''
-create table customers(
-    c_id int primary key,
-    c_fname varchar(50),
-    c_lname varchar(50),
-    c_email varchar(50)
-)
+# cur.execute('''
+# create table customers(
+#     c_id int primary key,
+#     c_fname varchar(50),
+#     c_lname varchar(50),
+#     c_email varchar(50)
+# )
     
 
 
-''')
+# ''')
 
-many_data = [
-    (1, 'arfan', 'shah', 'arfan@gmail.com'),
-    (2, 'zulfiqar', 'azam', 'zulfiqar@gmail.com'),
-    (3, 'tajwar', 'ali', 'tajwar@gmail.com'),
-    (4, 'munner', 'abbas', 'munner@gmail.com')
-]
+# many_data = [
+#     (1, 'arfan', 'shah', 'arfan@gmail.com'),
+#     (2, 'zulfiqar', 'azam', 'zulfiqar@gmail.com'),
+#     (3, 'tajwar', 'ali', 'tajwar@gmail.com'),
+#     (4, 'munner', 'abbas', 'munner@gmail.com')
+# ]
 
-cur.executemany("insert into customers values(?, ?, ?, ?)", many_data)
+# cur.executemany("insert into customers values(?, ?, ?, ?)", many_data)
+
+cur.execute("select * from customers")
+print(cur.fetchall())
+
+
 
 conn.commit()
 conn.close()
