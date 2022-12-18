854e69a286f61edad6529a812b7bce01a7b3d72e
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-12-18 15:55:23 +0600

    Added new file

--- numpyPractice2.py
+++ numpyPractice2.py
@@ -10,3 +10,14 @@ print(b)
 
 c = np.linspace(1.5, 2, num=10)
 print(c)
+
+arr1 = np.array([1, 2, 3, 'arfan', 1.4, True])
+print(arr1)
+print(type(arr1))
+
+lst = [1, 2, 3, 'arfan', 1.2, True]
+print(lst)
+print(type(lst))
+
+dict = {1: 'arfan', 2: 'shah', 3: 'zondrey'}
+print(dict, type(dict))
