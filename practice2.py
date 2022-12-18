fbb1b8de4e47c0b2d32f85725ac6722f202e2aca
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-12-18 10:55:27 +0600

    Added list comprehension

--- practice2.py
+++ practice2.py
@@ -1,6 +1,15 @@
 lst = []
+sqr_lst = []
 
 for i in range(10):
     lst.append(i)
 
 print(lst)
+
+for i in range(10):
+    sqr_lst.append(i*i)
+print(sqr_lst)
+
+#list comprehension
+l = [i*i for i in range(10)]
+print(l)
