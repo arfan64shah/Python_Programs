651043468860e2386c3b9261316b3d9d26831801
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-12-18 11:11:52 +0600

    Added

--- practice2.py
+++ practice2.py
@@ -13,3 +13,16 @@ print(sqr_lst)
 #list comprehension
 l = [i*i for i in range(10)]
 print(l)
+
+L = [(x, y) for x in [3, 5, 6] for y in [10, 15, 25] if y>3*x]
+
+print(L)
+
+s = {x for x in 'blabla na aula' if x not in ['a', 'f']}
+print(s)
+print(type(s))
+
+a = (1, 3, 4, 5, 6)
+b = (a, a, 3)
+print(b)
+print(type(b))
