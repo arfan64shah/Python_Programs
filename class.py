0b384f398658c67cdfd9c607ee311bf1eebad2d4
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2023-01-08 23:10:50 +0600

    Added new program

---
+++ class.py
@@ -0,0 +1,18 @@
+# print(type("Arfan Shah"))
+# print(type(42))
+class myclass:
+    print("My name is Arfan Shah")
+    # def odd(n: int) -> bool:
+    #     return n%2 != 0
+
+    # def main():
+    #     print(odd(3))
+
+    # if __name__ == "__main__":
+    #     main()
+a = myclass()
+b = myclass()
+
+print(a, b)
+
+print(a is b)
