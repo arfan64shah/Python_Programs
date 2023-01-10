e47e71ed96afd4a97ba9725ea341a6fa74739cf1
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2023-01-10 22:30:27 +0600

    Added new program

--- class1.py
+++ class1.py
@@ -1,5 +1,8 @@
 import math
 class point:
+    def __init__(self, x: float, y: float) -> None:
+        self.move(x, y)
+
     def move(self, x: float, y: float) -> None:
         self.x = x
         self.y = y
@@ -11,4 +14,16 @@ class point:
     def calculate_distance(self, other: "Point") -> float:
         return math.hypot(self.x - other.x, self.y - other.y)
 
-    
+point1 = point()
+point2 = point()
+
+point1.reset()
+point2.move(5, 0)
+
+print(point2.calculate_distance(point1))
+
+assert point2.calculate_distance(point1) == point1.calculate_distance(point2)
+
+point1.move(3, 4)
+print(point1.calculate_distance(point2))
+print(point1.calculate_distance(point1))
