edfe56e736b166d9791e4e11b8612d6297e28127
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2023-01-12 15:43:00 +0600

    Added new class

---
+++ class3.py
@@ -0,0 +1,27 @@
+class car:
+    pass
+
+honda = car()
+audi = car()
+lamborgani = car()
+ferrari = car()
+
+
+honda.speed = 200
+honda.color = 'black'
+honda.price = '40000 usd'
+
+
+audi.speed = 250
+audi.color = 'light black'
+audi.price = '50000 usd'
+
+lamborgani.speed = 400
+lamborgani.color = 'black'
+lamborgani.price = '200000 usd'
+
+ferrari.speed = 450
+ferrari.color = 'black'
+ferrari.price = '250000 usd'
+
+print('The speed of Ferrari is', ferrari.speed)
