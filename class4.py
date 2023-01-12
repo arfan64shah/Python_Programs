20499aff00c7b2958d17847f463864e238870f66
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2023-01-12 16:38:00 +0600

    Added new program for area calculation using opps

---
+++ class4.py
@@ -0,0 +1,29 @@
+import math
+
+class shape:
+    
+    def __init__(self, length, width, radius):
+        self.length = length
+        self.width = width
+        self.radius = radius
+
+class rectangle(shape):
+    
+    def area(self):
+        area = self.length * self.width
+        return area
+
+class square(shape):
+     def area(self):
+        area = self.length * self.width
+        return area
+
+class circle(shape):
+    def area(self):
+        return self.radius*self.radius * math.pi
+
+rect_area = rectangle(2, 3, 4)
+square_area = square(2, 3, 4)
+circle_area = circle(2, 3, 4)
+
+print(circle_area.area)
