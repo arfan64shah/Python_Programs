66ef2d07fdc3d8293b62855e49f4d241a6228714
Arfan Shah <arfan64shah@gmail.com>
2023-04-09 18:55:26 +0600

    Added arrays

---
+++ myarray.py
@@ -0,0 +1,17 @@
+import array as arr
+ar = arr.array('i', [1, 2, 3, 4, 5, 6, 7, 8])
+
+print(ar[::])
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8]
+print(lst[::-2])
+
+print(4 + 6 % 5)
+
+i = 1
+while True:
+    if i%3 == 0:
+        break
+    print(i)
+ 
+    i += 1
