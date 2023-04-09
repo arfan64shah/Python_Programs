7615cdffe38827932fdbfdf8949a03c37fa70d6b
Arfan Shah <arfan64shah@gmail.com>
2023-04-09 16:49:30 +0600

    Added another solution for finding largest number in an array

--- largestElementArray.py
+++ largestElementArray.py
@@ -1,5 +1,7 @@
 import array as arr
+#solved using two methods
 
+#solve using a normal way
 arr = arr.array('i', [44, 200, 3, 4, 5, 6, 7, 800])
 
 largest = arr[0]
@@ -8,3 +10,14 @@ for i in range(1, len(arr)):
     if arr[i] > largest:
         largest = arr[i]
 print(largest)
+
+#solve using function
+def largestNum(arr1):
+    largest = arr1[0]
+    for i in range(1, len(arr1)):
+        if arr1[i] > largest:
+            largest = arr1[i]
+    return largest
+arr1 = arr.array('i', [23000, 45, 65, 87, 90, 777, 888, 9999])
+
+print("The largest element in the array is ", largestNum(arr1))
