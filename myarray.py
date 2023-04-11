55ccccafca8ccbed1d6341278297cc296762bce8
Arfan Shah <arfan64shah@gmail.com>
2023-04-11 15:29:35 +0600

    Added new Programs

--- myarray.py
+++ myarray.py
@@ -1,4 +1,5 @@
 import array as arr
+import sys
 ar = arr.array('i', [1, 2, 3, 4, 5, 6, 7, 8])
 
 print(ar[::])
@@ -10,8 +11,17 @@ print(4 + 6 % 5)
 
 i = 1
 while True:
-    if i%3 == 0:
+    if i%3 == 0 :
         break
     print(i)
  
     i += 1
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+    if i == 3:
+        break
+else:
+    print(0)
