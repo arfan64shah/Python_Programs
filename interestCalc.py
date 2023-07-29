5a0e3e2abe715bc0e01cfa1e8d5a104959fd6ea3
Arfan Shah <arfan64shah@gmail.com>
2023-07-29 19:31:43 +0500

    Added basic calculator in python

---
+++ interestCalc.py
@@ -0,0 +1,9 @@
+amount = float(input("Enter initial amount: "))
+percentage = float(input("Enter interest percentage: "))
+time = float(input("Time after which you wanna find the interest: "))
+
+rate = percentage/100
+
+interest = amount * rate * time
+
+print("Interest is: ", interest)
