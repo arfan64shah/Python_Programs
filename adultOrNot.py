d6d2f6d29caa800d8890ce3b240c5342a38654a2
Arfan Shah <arfan64shah@gmail.com>
2023-07-29 20:07:43 +0500

    Added adult or not program

---
+++ adultOrNot.py
@@ -0,0 +1,10 @@
+#enter the age of the person
+age = int(input("Enter the age of the person: "))
+
+#check if he is adult or not using if and else statements
+
+if (age >= 18):
+    print("The person is an adult!")
+
+else:
+    print("The person is not an adult!")
