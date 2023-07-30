b3da3e86f11ddadccdd0dc62d6f24a3cd6719c88
Arfan Shah <arfan64shah@gmail.com>
2023-07-30 17:59:58 +0500

    Added for and while loops

---
+++ forWhile.py
@@ -0,0 +1,16 @@
+#for loop and append in the empty list
+numbers = []
+
+for x in range(1, 100, 5):
+    numbers.append(x)
+print(numbers)
+
+
+#while loop and appned in the list
+numbers1 = []
+count = 1
+while(count <= 100):
+    numbers1.append(count)
+    count += 5
+print(numbers1)
+    
