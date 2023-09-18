5a64a29ab7f6549e0b93b61c3368c9c6e6c66eb7
arfan64shah <arfan64shah@gmail.com>
2023-09-18 16:29:03 +0500

    Added

---
+++ stringg.py
@@ -0,0 +1,16 @@
+name = "My name is Arfan Shah and I am a Data Engineer"
+reverse = name[-1::-1]
+
+print(name)
+
+nam = name[0:21:2]
+print(nam)
+
+print(name[-1:-14:-1])
+
+for i in range(len(name)):
+    print(name[i])
+    
+    
+print(name.upper())
+print(reverse)
