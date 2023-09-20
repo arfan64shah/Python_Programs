946acf377e7fe608216d54647997cb8cff1ca699
arfan64shah <arfan64shah@gmail.com>
2023-09-20 13:05:00 +0500

    added

---
+++ string_to_list.py
@@ -0,0 +1,13 @@
+n = input("Enter words: ")
+
+l = n.split()
+
+print(l)
+
+# now print the continous input into list
+m = []
+
+for i in range(1, 4):
+    p = input("Enter word: ")
+    m.append(p)
+print(m)
