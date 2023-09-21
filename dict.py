cabc64c6483e2dc1fec2049353f5d6cc1e7ada45
arfan64shah <arfan64shah@gmail.com>
2023-09-21 10:03:09 +0500

    Added

---
+++ dict.py
@@ -0,0 +1,16 @@
+dic = {'fname' : 'arfan', 'lname': 'shah', 'age': 23, 'gender': 'male'}
+
+print(dic)
+
+print(dic['fname'] + ' ' + dic['lname'])
+
+
+k = dic.keys()
+print(k)
+
+v = dic.values()
+print(v)
+
+i = dic.items()
+print(i)
+
